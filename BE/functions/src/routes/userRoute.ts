1dcd71733890837ac0ec8a73337a854a28fa92e6
PNB309 <nguyen.phucbinh445@gmail.com>
2024-10-16 17:39:58 +0700

    refactor: @SEM-10 update project

--- BE/functions/src/routes/userRoute.ts
+++ BE/functions/src/routes/userRoute.ts
@@ -4,8 +4,5 @@ import * as userController from "../controllers/userController";
 const router = Router();
 router.get("/", userController.GetAllUser);
 router.post("/", userController.AddNewUser);
-router.post("/register", userController.RegisterUserAccount);
-router.post("/login", userController.LoginUser);
-router.post("/loginLink", userController.LoginUserWithLink);
 
 export default router;
--- BE/functions/src/services/userService.ts
+++ BE/functions/src/services/userService.ts
@@ -1,9 +1,6 @@
 import { User } from "../models/user";
 import { Firestore } from "firebase-admin/firestore";
-import * as admin from "firebase-admin";
-import { UserRecord } from "firebase-admin/auth";
-import { UserLoginRequestDto } from "../models/dto/User/userLoginRequestDto";
-import { UserLoginResponseDto } from "../models/dto/User/userLoginResonseDto";
+
 const db = new Firestore();
 export const createNewUser = async (
   data: Omit<User, "boards" | "id" | "createdAt" | "updatedAt">
@@ -26,40 +23,4 @@ export const getAllUser = async (): Promise<User[]> => {
     users.push(user);
   });
   return users;
-};
-export const registerUserAccount = async (
-  user: UserRegisterDto
-): Promise<UserRecord> => {
-  const userReturn = await admin.auth().createUser(user);
-  return userReturn;
-};
-export const loginUserAccount = async (
-  user: UserLoginRequestDto
-): Promise<UserLoginResponseDto> => {
-  const userLogin = await admin.auth().getUserByEmail(user.email);
-  return new UserLoginResponseDto(userLogin, "hello");
-};
-export const loginWithEmail = async (
-  user: UserLoginRequestDto
-): Promise<string> => {
-  const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for
-    // this URL must be whitelisted in the Firebase Console.
-    url: "https://www.facebook.com/",
-    // This must be true for email link sign-in.
-    handleCodeInApp: true,
-    iOS: {
-      bundleId: "com.example.ios",
-    },
-    android: {
-      packageName: "com.example.android",
-      installApp: true,
-      minimumVersion: "12",
-    },
-    // FDL custom domain.
-    dynamicLinkDomain: "coolapp.page.link",
-  };
-  return await admin
-    .auth()
-    .generateSignInWithEmailLink(user.email, actionCodeSettings);
 };
