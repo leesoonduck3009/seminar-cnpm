da35786f97c1fe1f768b3347e8c015e8dcd806ad
chip267 <tranthanhhien123bt@gmail.com>
2025-01-01 16:15:57 +0700

    fix add member

---
+++ FE/src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import LogoIcon from "@/components/icons/LogoIcon";
+import React from "react";
+
+const header = () => {
+  return (
+    <div className="flex items-center px-11 py-4 bg-[#579DFF]">
+      <LogoIcon />
+    </div>
+  );
+};
+
+export default header;
